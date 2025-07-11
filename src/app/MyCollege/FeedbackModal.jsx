3c1082a3f8e0b10efc37f7a7cc48523a98852a55
Md Shorif Uddin <mdshorifuddin463@gmail.com>
2025-07-11 23:08:47 +0600

    implement feedback system

---
+++ src/app/MyCollege/FeedbackModal.jsx
@@ -0,0 +1,103 @@
+'use client'
+import useAuth from '@/Hooks/useAuth';
+import useAxisSecure from '@/Hooks/useAxisSecure';
+import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import toast from 'react-hot-toast';
+
+const FeedbackModal = ({ setIsFeedback, admissionData }) => {
+  const { register, handleSubmit, reset } = useForm();
+  const {user} = useAuth();
+  const [rating, setRating] = useState(0);
+  const axiosSecure = useAxisSecure()
+
+  const onSubmit = async(data) => {
+    const feedbackData = {
+      email  : user.email,
+      college: admissionData.collegeName,
+      collegeId : admissionData.collegeId,
+      rating: rating,
+      description: data.description,
+      date: new Date(),
+    };
+
+    //   send feedback data to database
+      try {
+                const response = await axiosSecure.post('/api/feedback',feedbackData);
+                const result = response.data;
+                if(result.acknowledged && result.insertedId){
+
+                    setTimeout(() => {
+                         toast.success('Thanks for your feedback!');
+                    }, 200);
+
+                    reset();
+                      
+                }
+      } catch (error) {
+                console.log(error);
+                
+      }finally{
+          setRating(0);
+          setIsFeedback(false);
+
+      }
+
+  
+  };
+
+  return (
+    <dialog open className="modal modal-bottom sm:modal-middle">
+      <div className="modal-box">
+        <div className="flex justify-between items-center mb-4">
+          <h3 className="font-bold text-lg">Give Your Feedback</h3>
+          <button
+            onClick={() => setIsFeedback(false)}
+            className="btn btn-sm btn-circle btn-ghost text-xl"
+          >
+            ✕
+          </button>
+        </div>
+
+        {/* Rating */}
+        <div className="flex justify-center mb-4">
+          <div className="rating">
+            {[1, 2, 3, 4, 5].map((num) => (
+              <input
+                key={num}
+                type="radio"
+                name="rating"
+                className="mask mask-star bg-orange-400"
+                onChange={() => setRating(num)}
+                checked={rating === num}
+              />
+            ))}
+          </div>
+        </div>
+
+        {/* Description Form */}
+        <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
+          <div>
+            <label className="label">
+              <span className="text-xl font-semibold my-3">Description</span>
+            </label>
+            <textarea
+              {...register('description', { required: true })}
+              className="textarea textarea-bordered w-full"
+              rows={4}
+              placeholder="Write your feedback..."
+            ></textarea>
+          </div>
+
+          <div className="modal-action">
+            <button type="submit" className="btn btn-primary w-full">
+              Submit Feedback
+            </button>
+          </div>
+        </form>
+      </div>
+    </dialog>
+  );
+};
+
+export default FeedbackModal;
--- src/app/MyCollege/page.jsx
+++ src/app/MyCollege/page.jsx
@@ -1,22 +1,35 @@
 'use client'
 import useAdmission from '@/Hooks/useAdmission';
 import useAuth from '@/Hooks/useAuth';
-import React from 'react';
+import React, { useState } from 'react';
+import FeedbackModal from './FeedbackModal';
 
 const MyCollege = () => {
      const [admissions,refetch,isLoading] = useAdmission();
      const {user} = useAuth();
      const myAdmission = admissions.filter(data => data.email == user.email);
+     const [isFeedback,setIsFeedback] = useState(false)
+     const [admissionData , setAdmissionData] = useState(null)
 
 
     // handleFeedback function
     
-    const handleFeedback = (id) => {
+    const handleFeedback = (admission) => {
+          setIsFeedback(true);
+          setAdmissionData(admission)
           
     }
     return (
         <div className='w-full common_padding my-10'>
-      <h1 className='text-xl sm:text-2xl md:text-3xl lg:text-4xl capitalize font-semibold mt-4 mb-8 text-center'>Our Featured College</h1>
+              {
+                 isFeedback ?  
+                 
+                 <>
+                 <FeedbackModal admissionData={admissionData} setIsFeedback={setIsFeedback}></FeedbackModal>
+                 </> 
+                 :
+                  <>
+                   <h1 className='text-xl sm:text-2xl md:text-3xl lg:text-4xl capitalize font-semibold mt-4 mb-8 text-center'>Our Featured College</h1>
 
             <div className="my_college_list">
                      <div className="overflow-x-auto rounded-box border border-base-content/5 bg-base-100">
@@ -48,7 +61,7 @@ const MyCollege = () => {
                         <td> {admission.collegeName} </td>
                         <td> {admission.subject} </td>
                         <td> {admission.admitedDate} </td>
-                        <td>  <button className='secondary_btn btn-sm' onClick={() => handleFeedback(admission._id)}>  +Feedback </button> </td>
+                        <td>  <button className='secondary_btn btn-sm' onClick={() => handleFeedback(admission)}>  +Feedback </button> </td>
       </tr>
                  )
             })
@@ -57,6 +70,8 @@ const MyCollege = () => {
   </table>
 </div>
             </div>
+                  </>
+              }
 
         </div>
     );
